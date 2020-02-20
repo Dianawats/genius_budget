67e5d2feb17bc9fe8558b86a3d460b1b3e8c850c
Dianawats <dianakiznaki@gmail.com>
2020-02-20 15:40:39 +0300

    add diffrent module controllers in JS file

---
+++ app.js
@@ -0,0 +1,23 @@
+
+var budgetController = (function(){
+    //some code here
+
+})();
+
+
+var UIController = (function() {
+    //some code here
+
+})();
+
+
+var controller = (function(budgetCtrl, UICtrl) {
+    //code here
+
+})(budgetController, UIController);
+
+
+
+
+
+
