d92e367f5b573cacbf2cd061f97971739fa5a2e3
Dianawats <dianakiznaki@gmail.com>
2020-02-20 18:09:26 +0300

    create a private variable to store all data

--- app.js
+++ app.js
@@ -7,18 +7,30 @@ var budgetController = (function(){
 
 //UI CONTROLLER
 var UIController = (function() {
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
     
     return {
         getInput: function(){
 
             return {
-                type: document.querySelector('.add__type').value, //will be either inc or exp
-                description: document.querySelector('.add__description').value, 
-                value: document.querySelector('.add__value').value 
+                type: document.querySelector(DOMstrings.inputType).value, //will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value, 
+                value: document.querySelector(DOMstrings.inputValue).value 
             };
 
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
         }
-    } 
+
+    };
 
 })();
 
@@ -26,10 +38,13 @@ var UIController = (function() {
 //GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function(){
 
         //1. Get the field input data
         var input = UICtrl.getInput();
+        console.log(input);
 
         //2. Add the item to the controller
 
@@ -41,7 +56,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     }
     
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
 
