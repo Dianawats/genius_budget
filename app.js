f9784a5fb0333b3baa176a8c363d2c67a905bc5d
Dianawats <dianakiznaki@gmail.com>
2020-02-20 16:12:29 +0300

    set up events listener for input button

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
-
+//BUDGET CONTROLLER
 var budgetController = (function(){
     //some code here
 
 })();
 
 
+//UI CONTROLLER
 var UIController = (function() {
     //some code here
 
 })();
 
 
+//GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-    //code here
+
+    var ctrlAddItem = function(){
+
+        //1. Get the field input data
+
+        //2. Add the item to the controller
+
+        //3. Add new item to the user interface
+
+        //4. Calculate the budget
+
+        //5. Display the budget on the user interface
+
+    }
+    
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+
+        if (event.keyCode === 13 || event.which === 13){
+            ctrlAddItem();
+
+        }
+
+    });
+
+
 
 })(budgetController, UIController);
 
