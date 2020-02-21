b7c014d6f78eb274f5de40ced88362e5b08bf135
Dianawats <dianakiznaki@gmail.com>
2020-02-21 13:09:37 +0300

    Creating simple, reusable functions & sum all elements of an array using foreach mthd

--- app.js
+++ app.js
@@ -13,6 +13,16 @@ var budgetController = (function(){
         this.value = value;
     };
 
+    var calculateTotal = function(type) {
+        var sum = 0;
+        //loop over this array
+        data.allItems[type].forEach(function(cur) {
+            sum = sum += cur.value;
+        });
+        data.totals[type] = sum;
+
+    };
+
 
     var data = {
         allItems: {
@@ -21,8 +31,10 @@ var budgetController = (function(){
         },
         totals: {
             exp: 0,
-            inc: 0
-        }
+            inc: 0,
+        },
+        budget: 0,
+        percentage: -1
     };
 
 //return an object again that will contain all public methods
@@ -58,7 +70,34 @@ var budgetController = (function(){
             // Return the new element
             return newItem;
 
-        }
+        },
+
+        calculateBudget: function() {
+            //calculate the total incomes and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+
+            // Calculate the budget: income-expenses
+            data.budget = data.totals.inc - data.totals.exp;
+
+            // calculate the percentange of income that we spent
+            if (data.totals.inc > 0) {
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+            
+        }, 
+
+        getBudget: function() {
+            return {
+                budget: data.budget,
+                totalTnc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            };
+
+        },
 
     };
     
@@ -157,10 +196,13 @@ var controller = (function(budgetCtrl, UICtrl) {
     var updateBudget = function() {
 
         //1. Calculate the budget
+        budgetCtrl.calculateBudget();
 
         //2. Return the budget
+        var budget = budgetCtrl.getBudget();
 
         //3. Display the budget on the user interface
+        console.log(budgetCtrl);
 
     };
 
